80940afe49d03536acc473a5e12684610a68e8bd
ganesh25153F <vatambetiganesh2@gmail.com>
2026-07-02 21:23:20 +0530

    Integrate Flask app with ScientificCalculator

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from calculator import ScientificCalculator
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run()
 
 if __name__ == "__main__":
     app = ScientificCalculator()
